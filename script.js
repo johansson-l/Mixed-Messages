c1e1cd2e553709a55a395c5061f04e8d3bf01bc3
Linus Johansson <lsjn88@gmail.com>
2021-12-16 14:45:30 +0100

    Initiated random functions

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 console.log("Mixed Messages");
+
+// Return random temperature centigrade
+const returnTemp = () => {
+    const temp = Math.floor(Math.random() * 35);
+    return temp;
+};
+
+// Return random wind [m/s]
+const returnWind = () => {
+    const wind = Math.floor(Math.random() * 15);
+    return wind;
+};
+
+// Checks if it's raining. If so, return precipitation [mm]
+const isRaining = (value) => {
+    const precipitation = 0;
+    // If value is true, randomize the precipitation.
+    if (value) {
+        return precipitation = Math.floor(Math.random() * 20);
+    } else {
+        return precipitation;
+    };
+};
